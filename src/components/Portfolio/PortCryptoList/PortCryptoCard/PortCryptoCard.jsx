1e7a482b370d8ee8a5e848d5731128140c9074d4
Ashlee <ashleeryan@outlook.com>
2021-03-22 20:33:05 +0000

    User can now input their holdings and portfolio worth displays

--- src/components/Portfolio/PortCryptoList/PortCryptoCard/PortCryptoCard.jsx
+++ src/components/Portfolio/PortCryptoList/PortCryptoCard/PortCryptoCard.jsx
@@ -1,10 +1,27 @@
 import { StylesProvider } from '@material-ui/styles'
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import styles from "./PortCryptoCard.module.scss"
 
 const PortCryptoCard = (props) => {
 
-    const {amount, setAmount} = useState(0)
+    
+    const [amount, setAmount] = useState(0)
+   
+
+    const displayAmount = () => {
+        const intValue = (parseInt(inputAmount.current.value))
+        setAmount(intValue)
+        console.log(inputAmount)
+        console.log(inputAmount.current.value)
+    }
+    
+    const inputAmount = useRef(null)
+
+    const cryptoWorth = (num, price)=> {
+
+        return num * price
+
+    }
 
     const placeholderText = `Input amount of ${props.coin.name}`
 
@@ -12,8 +29,8 @@ const PortCryptoCard = (props) => {
         <div className={styles.cardDiv}>
             <h1>{props.coin.name}</h1>
             <p>Current Price: £{props.coin.current_price}</p>
-            <input value={amount} placeholder={placeholderText}></input>
-            <p>Your {props.coin.name} is worth {amount * props.coin.current_price}</p>
+            <input onChange={displayAmount} type="number" ref={inputAmount}  placeholder={placeholderText}></input>
+            <p>Your {props.coin.name} is worth £{amount * props.coin.current_price}</p>
         </div>
     )
 }
