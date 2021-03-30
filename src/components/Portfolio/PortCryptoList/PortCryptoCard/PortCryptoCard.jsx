e8638ff459b70331f358135524653b55ecce04a6
Ashlee <ashleeryan@outlook.com>
2021-03-30 21:31:15 +0100

    replaced portfolio card with MUI card

--- src/components/Portfolio/PortCryptoList/PortCryptoCard/PortCryptoCard.jsx
+++ src/components/Portfolio/PortCryptoList/PortCryptoCard/PortCryptoCard.jsx
@@ -1,6 +1,7 @@
 import { StylesProvider } from '@material-ui/styles'
 import React, { useState, useRef } from 'react'
 import styles from "./PortCryptoCard.module.scss"
+import { Card, Grid, Container } from "@material-ui/core"
 import { LineChart, Line } from 'recharts';
 
 const PortCryptoCard = (props) => {
@@ -32,17 +33,23 @@ const PortCryptoCard = (props) => {
     const placeholderText = `Input amount of ${props.coin.name}`
 
     return (
-        <div className={styles.cardDiv}>
-            <h1>{props.coin.name}</h1>
-            <p>Current Price: £{props.coin.current_price}</p>
-            {/* <p>1hr Change: {props.coin.price_change_percentage_1h_in_currency}%</p> */}
-            <p>24hr Change: {props.coin.price_change_percentage_24h}%</p>
-            <input onChange={displayAmount} type="number" ref={inputAmount}  placeholder={placeholderText}></input>
-            <p>Your {props.coin.name} is worth £{amount * props.coin.current_price}</p>
-            {/* <LineChart width={400} height={400} data={sparkLineData}>
-                    <Line type="monotone" dataKey="value" stroke="#8884d8" />
-            </LineChart> */}
-        </div>
+        <Container spacing={2}>
+        <Grid container>
+            <Grid item xs={12}>
+            <Card variant="outlined" raised>
+                <h1>{props.coin.name}</h1>
+                <p>Current Price: £{props.coin.current_price}</p>
+                {/* <p>1hr Change: {props.coin.price_change_percentage_1h_in_currency}%</p> */}
+                <p>24hr Change: {props.coin.price_change_percentage_24h}%</p>
+                <input onChange={displayAmount} type="number" ref={inputAmount}  placeholder={placeholderText}></input>
+                <p>Your {props.coin.name} is worth £{amount * props.coin.current_price}</p>
+                {/* <LineChart width={400} height={400} data={sparkLineData}>
+                        <Line type="monotone" dataKey="value" stroke="#8884d8" />
+                </LineChart> */}
+            </Card>
+            </Grid>
+            </Grid>
+            </Container>
     )
 }
 
